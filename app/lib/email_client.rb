17bafa28e5edf9ec07fb05396af0330e5702a5b6
David McGinn <davidmcginn@gmail.com>
2020-05-21 20:33:52 +0100

    Change sendgrid implementation

--- app/lib/email_client.rb
+++ app/lib/email_client.rb
@@ -8,14 +8,41 @@ class EmailClient
   end
 
   def send
-    from = Email.new(email: 'dave@now-u.com')
-    to = Email.new(email: @email)
-    subject = 'Now-u Account Login'
-    content = Content.new(type: 'text/plain', value: @body)
-    mail = Mail.new(from, subject, to, content)
-
+    data = {
+      "personalizations" => [
+        {
+          "to" => [
+            {
+              "email" => @email
+            }
+          ],
+          "subject" => "Now-u Account Login"
+        }
+      ],
+      "from" => {
+        "email" => "dave@now-u.com"
+      },
+      "content" => [
+        {
+          "type" => "text/plain",
+          "value" => @body
+        }
+      ]
+    }
     sg = SendGrid::API.new(api_key: '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')
-    response = sg.client.mail._('send').post(request_body: mail.to_json)
+    response = sg.client.mail._("send").post(request_body: data)
     Rails.logger.info "User registration/login email sent; Email: #{@email}, Body: #{@body}; Response code: #{response.status_code}, body: #{response.body}, header: #{response.headers}"
   end
+
+  # def send
+  #   from = Email.new(email: 'dave@now-u.com')
+  #   to = Email.new(email: @email)
+  #   subject = 'Now-u Account Login'
+  #   content = Content.new(type: 'text/plain', value: @body)
+  #   mail = Mail.new(from, subject, to, content)
+  #
+  #   sg = SendGrid::API.new(api_key: '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')
+  #   response = sg.client.mail._('send').post(request_body: mail.to_json)
+  #   Rails.logger.info "User registration/login email sent; Email: #{@email}, Body: #{@body}; Response code: #{response.status_code}, body: #{response.body}, header: #{response.headers}"
+  # end
 end
