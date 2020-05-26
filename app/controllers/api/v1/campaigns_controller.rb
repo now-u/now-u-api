8121d4281e18ebd26b3bc01f6cfe11d3a7f8b6df
David McGinn <davidmcginn@gmail.com>
2020-05-26 22:28:46 +0100

    Return partners and goals with campaigns

--- app/controllers/api/v1/campaigns_controller.rb
+++ app/controllers/api/v1/campaigns_controller.rb
@@ -1,8 +1,12 @@
 class Api::V1::CampaignsController < ApplicationController
   def index
     data = { data: Campaign.all }
+    data = data.to_json(
+      methods: [:general_partners, :campaign_partners, :sdgs],
+      include: { actions: {}, learning_topics: {} }
+    )
 
-    render json: data.to_json(include: [:actions]), status: :ok
+    render json: data, status: :ok
   end
 
   def show
--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -7,4 +7,16 @@ class Campaign < ApplicationRecord
   has_many :organisations, :through => :partnerships
   has_many :campaign_goals
   has_many :goals, :through => :campaign_goals
+
+  def sdgs
+    goals.where('type = ?', 'United Nations Sustainable Development Goal')
+  end
+
+  def general_partners
+    organisations.where('partnerships.type = ?', 'general')
+  end
+
+  def campaign_partners
+    organisations.where('partnerships.type = ?', 'campaign')
+  end
 end
