9d4dc9af6d27ddca9b0bbc519f6bd66537fdda5d
Jelgar <jamesnelgar@gmail.com>
2020-05-29 00:20:32 +0100

    Oh deary me

---
+++ entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+unset BUNDLE_PATH
+unset BUNDLE_BIN
+
+# Remove a potentially pre-existing server.pid for Rails.
+rm -f /now-u-api/tmp/pids/server.pid
+
+# Then exec the container's main process (what's set as CMD in the Dockerfile).
+exec "$@"
