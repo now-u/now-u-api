e4075f1da1846b52b07d9e95afd02275473b410d
David McGinn <davidmcginn@gmail.com>
2020-06-01 18:55:30 +0100

    Encode registration url

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -20,6 +20,8 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def email_body(user)
-    "<html><head></head><body>Thanks for registering! You can login with the following link:<br /><br /> <a href=\"https://nowu.page.link/?link=https://now-u.com/loginMobile?email=#{user.email}&token=#{user.token}&apn=com.nowu.app\">Click me</a></body></html>"
+    url = "https://now-u.com/loginMobile?email=#{user.email}&token=#{user.token}&apn=com.nowu.app"
+    url = CGI::escape(url)
+    "<html><head></head><body>Thanks for registering! You can login with the following link:<br /><br /> <a href=\"https://nowu.page.link/?link=#{url}">Click me</a></body></html>"
   end
 end
