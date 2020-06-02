5583241c63f0937e72f28b46bf8d5ea94cb9b793
Jelgar <jamesnelgar@gmail.com>
2020-06-03 00:31:55 +0100

    Fixing already running error

--- docker/entrypoint-deploy.sh
+++ docker/entrypoint-deploy.sh
@@ -5,7 +5,7 @@ unset BUNDLE_PATH
 unset BUNDLE_BIN
 
 # Remove a potentially pre-existing server.pid for Rails.
-rm -f /now-u-api/tmp/pids/server.pid
+rm -f /usr/src/app/tmp/pids/server.pid
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 
