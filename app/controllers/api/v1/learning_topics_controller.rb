70dec77cc4e5b82987009fd92a1a734d6d3f7417
David McGinn <davidmcginn@gmail.com>
2020-06-08 21:04:58 +0100

    returnig learning resources with topics

--- app/controllers/api/v1/learning_topics_controller.rb
+++ app/controllers/api/v1/learning_topics_controller.rb
@@ -1,5 +1,14 @@
 class Api::V1::LearningTopicsController < ApplicationController
   def index
-    render json: { data: Campaign.find(params[:id]).learning_topics }, status: :ok
+    render json: learning_topics, status: :ok
+  end
+
+  private
+
+  def learning_topics
+    data = { data: Campaign.find(params[:id]).learning_topics }
+    data.to_json(
+      include: [:learning_resources]
+    )
   end
 end
