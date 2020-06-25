939e253fbabaab799aaef92aa71e45517bdcb7c6
Stephan Ellenberger <53180713+stellenberger@users.noreply.github.com>
2020-06-25 11:54:13 +0100

    Update 20200624093115_create_text_sections.rb
    Changed blog_id to blog_article_id

--- db/migrate/20200624093115_create_text_sections.rb
+++ db/migrate/20200624093115_create_text_sections.rb
@@ -2,7 +2,7 @@ class CreateTextSections < ActiveRecord::Migration[5.2]
   def change
     create_table :text_sections do |t|
       t.text :content
-      t.integer :blog_id
+      t.integer :blog_article_id
       t.integer :appearance_order
       t.timestamps
     end
