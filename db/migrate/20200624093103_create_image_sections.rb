2c281a4b647041786e9e5d75ffabc137710eb105
Stephan Ellenberger <53180713+stellenberger@users.noreply.github.com>
2020-06-25 11:54:37 +0100

    Update 20200624093103_create_image_sections.rb
    changed blog_id to blog_article_id

--- db/migrate/20200624093103_create_image_sections.rb
+++ db/migrate/20200624093103_create_image_sections.rb
@@ -2,7 +2,7 @@ class CreateImageSections < ActiveRecord::Migration[5.2]
   def change
     create_table :image_sections do |t|
       t.string :img_url
-      t.integer :blog_id
+      t.integer :blog_article_id
       t.integer :appearance_order
       t.timestamps
     end
