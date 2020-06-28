30237502d8398947468cc337b9052a8c0a20ac6d
David McGinn <davidmcginn@gmail.com>
2020-06-28 19:24:06 +0100

    Add comment field to user_actions

---
+++ db/migrate/20200628181406_add_comment_to_user_actions.rb
@@ -0,0 +1,5 @@
+class AddCommentToUserActions < ActiveRecord::Migration[5.2]
+  def change
+    add_column :user_actions, :comment, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_28_180339) do
+ActiveRecord::Schema.define(version: 2020_06_28_181406) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -182,6 +182,7 @@ ActiveRecord::Schema.define(version: 2020_06_28_180339) do
     t.string "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "comment"
   end
 
   create_table "user_campaigns", force: :cascade do |t|
