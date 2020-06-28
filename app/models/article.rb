693f3f9e3524474f22bf4ee790049176e4029816
David McGinn <davidmcginn@gmail.com>
2020-06-28 19:55:49 +0100

    Create and use active scope for articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ApplicationRecord
   belongs_to :campaign
   belongs_to :action
+
+  scope :active, -> { where('enabled IS TRUE AND release_date IS NULL').or(where('enabled IS TRUE AND ? > release_date', DateTime.now)) }
 end
