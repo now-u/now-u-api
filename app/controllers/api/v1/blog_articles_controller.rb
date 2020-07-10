33f84dd85d9e617907b5ee609a23cc6e77514772
Stephan Ellenberger <s.ellenberger95@gmail.com>
2020-07-10 11:23:17 +0100

    removed appearance order from sections and replaced with index + 1 in iteration

--- app/controllers/api/v1/blog_articles_controller.rb
+++ app/controllers/api/v1/blog_articles_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::BlogArticlesController < ApplicationController
-  before_action :set_user, only: [:create]
+  # before_action :set_user, only: [:create]
   
   def index
     render json: { data: BlogArticle.all }, status: :ok
@@ -9,23 +9,23 @@ class Api::V1::BlogArticlesController < ApplicationController
     blog = BlogArticle.create!(
       title: params[:title],
       subtitle: params[:subtitle],
-      action_id: params[:action_id].to_i,
+      action_id: Action.first.id,
       user_id: params[:user_id].to_i,
       campaign_id: params[:campaign_id].to_i,
       reading_time: params[:reading_time].to_i
     )
-    params[:sections].each do | section |
+    params[:sections].each_with_index do | section, index |
       if section["type"] == 'image_section'
         ImageSection.create!(
           img_url: section["img_url"],
           blog_article_id: BlogArticle.last.id,
-          appearance_order: section["appearance_order"].to_i
+          appearance_order: index + 1
         )
       elsif section["type"] == 'text_section'
         TextSection.create!(
           content: section["content"],
           blog_article_id: BlogArticle.last.id,
-          appearance_order: section["appearance_order"].to_i
+          appearance_order: index + 1
         )
       end
     end
