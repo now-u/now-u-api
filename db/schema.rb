fe2238dd1abc728e11922f1f703866b1111b13c6
Stephan Ellenberger <s.ellenberger95@gmail.com>
2020-07-18 17:32:17 +0100

    added header image to blog articles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_15_155249) do
+ActiveRecord::Schema.define(version: 2020_07_18_163046) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 2020_07_15_155249) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "enabled", default: false
+    t.string "header_image"
   end
 
   create_table "blog_comments", force: :cascade do |t|
