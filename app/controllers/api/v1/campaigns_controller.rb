137b0daf39622d8e06d0a152beea409575ee9602
David McGinn <davidmcginn@gmail.com>
2020-07-22 16:53:59 +0100

    Accept old param in campaigns index endpoint

--- app/controllers/api/v1/campaigns_controller.rb
+++ app/controllers/api/v1/campaigns_controller.rb
@@ -1,9 +1,19 @@
 class Api::V1::CampaignsController < ApplicationController
   def index
-    render json: Campaign.active.all, root: 'data', status: :ok, adapter: :json
+    render json: campaign_data, root: 'data', status: :ok, adapter: :json
   end
 
   def show
     render json: Campaign.find(params[:id]), root: 'data', status: :ok, adapter: :json
+  end
+
+  private
+
+  def campaign_data
+    if params[:old]
+      Campaign.where('end_date < ?', DateTime.now).all
+    else
+      Campaign.active.all
+    end
   end
 end
