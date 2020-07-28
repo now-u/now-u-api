a13112f08985d7d580b8f834c423470d8ed29eb9
David McGinn <davidmcginn@gmail.com>
2020-07-28 19:39:06 +0100

    Return infographic_url with CampaignSerializer

--- app/serializers/campaign_serializer.rb
+++ app/serializers/campaign_serializer.rb
@@ -3,7 +3,7 @@ class CampaignSerializer < ActiveModel::Serializer
              :video_link, :description_web, :enabled, :start_date, :end_date,
              :short_name, :number_of_campaigners, :general_partners, :campaign_partners,
              :sdgs, :key_aims,
-             :actions, :learning_topics
+             :actions, :learning_topics, :infographic_url
 
   def actions
     self.object.actions.active
