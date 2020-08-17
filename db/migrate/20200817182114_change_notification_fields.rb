a7dea8e654b8e27f498328be818d6ed6961bedc8
David McGinn <davidmcginn@gmail.com>
2020-08-17 19:26:10 +0100

    Migration to change notifications fields

---
+++ db/migrate/20200817182114_change_notification_fields.rb
@@ -0,0 +1,8 @@
+class ChangeNotificationFields < ActiveRecord::Migration[5.2]
+  def change
+    add_column :notifications, :title, :string
+    add_column :notifications, :image, :string
+    add_column :notifications, :body, :text
+    remove_column :notifications, :text
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_12_191335) do
+ActiveRecord::Schema.define(version: 2020_08_17_182114) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -142,10 +142,12 @@ ActiveRecord::Schema.define(version: 2020_08_12_191335) do
   end
 
   create_table "notifications", force: :cascade do |t|
-    t.string "text"
     t.string "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
+    t.string "image"
+    t.text "body"
   end
 
   create_table "offers", force: :cascade do |t|
