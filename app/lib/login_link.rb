d22ecffed5a0b852a11b22d0296c19e29ed38053
David McGinn <davidmcginn@gmail.com>
2020-09-16 21:45:16 +0100

    Create class for login link logic

---
+++ app/lib/login_link.rb
@@ -0,0 +1,26 @@
+class TokenNotPresentError < StandardError; end
+
+class LoginLink
+  def initialize(token:, platform: nil, version: nil)
+    @token = token
+    @platform = platform
+    @version = version
+  end
+
+  def link
+    raise TokenNotPresentError, 'Token is nil' unless @token
+    return "com.nowu.app://loginMobile?token=#{@token}" if ios13?
+
+    "https://nowu.page.link/?link=https://now-u.com/loginMobile?token%3D#{@token}&apn=com.nowu.app&isi=1516126639&ibi=com.nowu.app"
+  end
+
+  private
+
+  def ios13?
+    return false unless @platform && @version
+
+    return true if (@platform.downcase == 'ios') && (@version.downcase.to_s == '13')
+
+    false
+  end
+end
