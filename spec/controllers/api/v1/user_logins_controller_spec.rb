ffc64d65fa6aea2a08ee13059cdea91a51a7ca29
David McGinn <davidmcginn@gmail.com>
2020-10-01 13:57:21 +0100

    Fix broken tests

--- spec/controllers/api/v1/user_logins_controller_spec.rb
+++ spec/controllers/api/v1/user_logins_controller_spec.rb
@@ -4,43 +4,43 @@ RSpec.describe Api::V1::UserLoginsController, type: :controller do
   describe '#create' do
     let(:verified) { false }
     let(:user) { User.create!(email: 'ok@ok.com', token: 'abc1234567', verified: verified) }
-    let(:token) { 'abc1234567' }
+    let(:user_token) { user.user_tokens.create! } # temporary token
+    # let(:token) { 'abc1234567' }
 
     subject(:login_user) do
-      post :create, params: { token: token, email: user.email }
+      post :create, params: { token: user_token.token, email: user.email }
     end
 
-    it 'changes the token' do
-      expect { login_user }.to change { user.reload.token }
+    it 'does not change the permanent token' do
+      expect { login_user }.not_to change { user.reload.token }
     end
 
-    it 'returns the new token' do
-      login_user
+    context 'when temporary token is valid' do
+      it 'returns the permanent token' do
+        login_user
 
-      expect(JSON.parse(response.body)).to eq({ 'data' => { 'token' => user.reload.token } })
+        expect(JSON.parse(response.body)).to eq({ 'data' => { 'token' => user.token } })
+      end
+
+      it 'sets verified flag to true' do
+        expect { login_user }.to change { user.reload.verified }.from(false).to(true)
+      end
+
+      it 'returns 200 status' do
+        login_user
+
+        expect(response.status).to eq(200)
+      end
     end
 
-    it 'returns 200 status' do
-      login_user
-
-      expect(response.status).to eq(200)
-    end
-
-    context 'when token is invalid' do
-      let(:token) { 'zzzzzzz' }
-
+    context 'when temporary token is invalid' do
       before do
+        user_token.expire!
         login_user
       end
 
       it 'returns unauthorised response' do
         expect(response.status).to eq(401)
-      end
-    end
-
-    context 'when user is not verified' do
-      it 'sets verified flag to true' do
-        expect { login_user }.to change { user.reload.verified }.from(false).to(true)
       end
     end
   end
