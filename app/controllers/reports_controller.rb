d2aaa61bb1170faca3b868067e6ab97d01771c06
David McGinn <davidmcginn@gmail.com>
2020-10-24 12:24:34 +0100

    Reports interface

---
+++ app/controllers/reports_controller.rb
@@ -0,0 +1,11 @@
+class ReportsController < ActionController::Base
+  def index
+    if params[:key] == 'feb89cc6e9b86f6d81c991cebb8205658e2226bf718ece30b8575755f2be'
+      status = params[:status] || 'active'
+      @report_data = Reports::CampaignReports.new(status: status).report
+      render template: 'reports'
+    else
+      render json: { error: 'No access' }, status: 401
+    end
+  end
+end
