7afe98367543d8909cc4352fb9fe10e484436531
David McGinn <davidmcginn@gmail.com>
2020-11-12 17:56:40 +0000

    Change action_steps index column name

--- db/migrate/20201110200317_create_action_steps.rb
+++ db/migrate/20201110200317_create_action_steps.rb
@@ -2,7 +2,7 @@ class CreateActionSteps < ActiveRecord::Migration[5.2]
   def change
     create_table :action_steps do |t|
       t.integer :action_id
-      t.integer :action_index, default: 0
+      t.integer :index, default: 0
       t.string :text
 
       t.timestamps
