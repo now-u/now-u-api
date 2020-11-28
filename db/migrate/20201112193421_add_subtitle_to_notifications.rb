63d1336e579101c7e516a37abf6bd795df1ed6d6
David McGinn <davidmcginn@gmail.com>
2020-11-28 09:53:40 +0000

    Add subtitle to notifications

---
+++ db/migrate/20201112193421_add_subtitle_to_notifications.rb
@@ -0,0 +1,5 @@
+class AddSubtitleToNotifications < ActiveRecord::Migration[5.2]
+  def change
+    add_column :notifications, :subtitle, :text
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_05_204106) do
+ActiveRecord::Schema.define(version: 2020_11_12_193421) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -148,6 +148,7 @@ ActiveRecord::Schema.define(version: 2020_11_05_204106) do
     t.string "title"
     t.string "image"
     t.text "body"
+    t.text "subtitle"
   end
 
   create_table "offers", force: :cascade do |t|
