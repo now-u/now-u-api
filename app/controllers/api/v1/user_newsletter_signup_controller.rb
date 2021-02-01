d6200759d56316d212f4c02f02ccfae4309975c7
antonzaharia <zaharia_anton@yahoo.com>
2021-02-01 17:01:25 +0000

    user_newsletter_toggle_endpoint

--- app/controllers/api/v1/user_newsletter_signup_controller.rb
+++ app/controllers/api/v1/user_newsletter_signup_controller.rb
@@ -17,4 +17,15 @@ class Api::V1::UserNewsletterSignupController < ApplicationController
 
     render json: { data: user_response }, status: :ok
   end
+
+  def toggle
+    client = MailingListClient.new
+    if client.subscriber(list_id: MAILCHIMP_LIST_ID, email_address: @user.email).status == 'subscribed'
+      client.remove_from_list(list_id: MAILCHIMP_LIST_ID, email_address: @user.email)
+      @user.update(newsletter: false)
+    else
+      client.add_to_list(list_id: MAILCHIMP_LIST_ID, email_address: @user.email, name: @user.full_name)
+      @user.update(newsletter: true)
+    end
+  end
 end
