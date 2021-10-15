068e695786e0aeb1db02f631680eda1a265aac58
Stephan Ellenberger <stephan.ellenberger@digital.education.gov.uk>
2021-10-15 16:52:42 +0100

    Campaign and campaign action scope specs

--- spec/models/campaign_action_spec.rb
+++ spec/models/campaign_action_spec.rb
@@ -7,28 +7,34 @@ describe CampaignAction do
 
   it { should be_valid }
 
+  before do
+    3.times { create(:campaign_action) }
+    2.times { create(:campaign_action, of_the_month: true) }
+    1.times { create(:campaign_action, recommended: true) }
+  end
+
   context "scopes" do
     describe "filter_by_cause" do
       it "takes an array of cause id's and returns associated records" do
-        
+        expect(described_class.filter_by_cause([1, 2])).to eq []
       end
     end
 
     describe "limit" do
       it "takes a limit and returns last n records" do
-        
+        expect(described_class.limit(2).length).to eq 2
       end
     end
 
     describe "of_the_month" do
       it "returns of_the_month featured actions" do
-        
+        expect(described_class.of_the_month(true).length).to eq 2
       end
     end
 
     describe "recommended" do
       it "returns recommended featured actions" do
-        
+        expect(described_class.recommended(true).length).to eq 1
       end
     end
   end
