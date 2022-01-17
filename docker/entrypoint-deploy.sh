7e633e006bf10a21f315c014f01ab9f0838cbb34
Stephan Ellenberger <stephan.ellenberger@digital.education.gov.uk>
2022-01-17 10:58:53 +0000

    Revert back to using rails 5.2.3 and update Dockerfile.deploy

--- docker/entrypoint-deploy.sh
+++ docker/entrypoint-deploy.sh
@@ -1,15 +1,14 @@
-#!/bin/bash
+#!/bin/sh
 set -e
 
-# unset BUNDLE_PATH
-# unset BUNDLE_BIN
+unset BUNDLE_PATH
+unset BUNDLE_BIN
 
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /usr/src/app/tmp/pids/server.pid
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
-
-#bundle install
+bundle install --system
 
 # echo "Bundling gems"
 # bundler install --without development test --jobs 8 --retry 3
