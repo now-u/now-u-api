5dab5efb6b4bdd08cbb9efecba11eaa10db594e4
Stephan Ellenberger <stephan.ellenberger@digital.education.gov.uk>
2022-01-17 09:07:38 +0000

    Fix nokogiri dependencies for alpine

--- docker/entrypoint-deploy.sh
+++ docker/entrypoint-deploy.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 set -e
 
-# unset BUNDLE_PATH
-# unset BUNDLE_BIN
+unset BUNDLE_PATH
+unset BUNDLE_BIN
 
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /usr/src/app/tmp/pids/server.pid
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
-bundle check || bundle install --jobs 20 --retry 5
+bundle install --system
 
 # echo "Bundling gems"
 # bundler install --without development test --jobs 8 --retry 3
