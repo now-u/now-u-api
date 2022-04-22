e4b196369105ee665e16e10b0ade90030297b4b3
stellenberger <s.ellenberger95@gmail.com>
2022-04-22 18:14:21 +0100

    Use environment variables for sentry

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,5 +1,5 @@
 Sentry.init do |config|
-  config.dsn = 'https://e43549eae9bb4f81891972b87e79b5ee@o1209445.ingest.sentry.io/6356423'
+  config.dsn = ENV['SENTRY_ENDPOINT']
   config.breadcrumbs_logger = [:sentry_logger, :http_logger]
 
   config.traces_sample_rate = 0.01
