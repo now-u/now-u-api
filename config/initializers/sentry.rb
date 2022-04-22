10dc8708ed5a6fe1b7f09a9caf5128b76873f4ec
stellenberger <s.ellenberger95@gmail.com>
2022-04-22 18:12:19 +0100

    Change sample rate

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -2,11 +2,5 @@ Sentry.init do |config|
   config.dsn = 'https://e43549eae9bb4f81891972b87e79b5ee@o1209445.ingest.sentry.io/6356423'
   config.breadcrumbs_logger = [:sentry_logger, :http_logger]
 
-  # To activate performance monitoring, set one of these options.
-  # We recommend adjusting the value in production:
-  config.traces_sample_rate = 0.5
-  # or
-  config.traces_sampler = lambda do |context|
-    0.5
-  end
+  config.traces_sample_rate = 0.01
 end
