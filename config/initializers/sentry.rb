51f50f02c3a9c598e72702e2215004288ebc7dd6
stellenberger <s.ellenberger95@gmail.com>
2022-06-27 19:16:30 +0100

    Update sentry config

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,9 +1,7 @@
 Sentry.init do |config|
-  if Rails.env.production?
-    config.dsn = ENV['SENTRY_PRODUCTION_DSN']
-  else Rails.env.staging?
-    config.dsn = ENV['SENTRY_STAGING_DSN']
-  end
+  config.dsn = ENV['SENTRY_DSN']
+
+  config.environment = Rails.env
   config.breadcrumbs_logger = [:sentry_logger, :http_logger]
 
   config.traces_sample_rate = 0.01
