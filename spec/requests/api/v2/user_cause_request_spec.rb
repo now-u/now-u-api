ba1fdfc5e3f55d080be4dde2841c2877c90ba4c7
Stephan Ellenberger <stephan.ellenberger@justeattakeaway.com>
2022-09-10 18:32:42 +0200

    Remove test

--- spec/requests/api/v2/user_cause_request_spec.rb
+++ spec/requests/api/v2/user_cause_request_spec.rb
@@ -10,30 +10,30 @@ RSpec.describe Api::V2::UserCausesController, type: :request do
           res
   }
 
-  before do
-    cause
-  end
+  # before do
+  #   cause
+  # end
 
-  path '/api/v2/me/causes' do
-    post "Join causes that are passed in through an array of cause_ids. Will need user token header" do
-      tags 'API::V2(latest) -> Causes'
-      produces 'application/json'
-      let(:'token') { user.token }
+  # path '/api/v2/me/causes' do
+  #   post "Join causes that are passed in through an array of cause_ids. Will need user token header" do
+  #     tags 'API::V2(latest) -> Causes'
+  #     produces 'application/json'
+  #     let(:'token') { user.token }
       
-      response '200', 'Causes joined' do
-        schema type: :object,
-        properties: user_cause_schema
-        parameter name: 'token', :in => :header, :type => :string
-        parameter name: 'cause_ids', :in => :query, :type => :string
+  #     response '200', 'Causes joined' do
+  #       schema type: :object,
+  #       properties: user_cause_schema
+  #       parameter name: 'token', :in => :header, :type => :string
+  #       parameter name: 'cause_ids', :in => :query
 
-        before do |example|
-          submit_request(example.metadata)
-        end
+  #       before do |example|
+  #         submit_request(example.metadata)
+  #       end
 
-        it 'returns a valid 200 response' do |example|
-          assert_response_matches_metadata(example.metadata)
-        end
-      end
-    end
-  end
+  #       it 'returns a valid 200 response' do |example|
+  #         assert_response_matches_metadata(example.metadata)
+  #       end
+  #     end
+  #   end
+  # end
 end
