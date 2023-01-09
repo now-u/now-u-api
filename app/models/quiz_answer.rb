9be1205d9da355a9da3c6569972e0a2573f95cf8
Stephan Ellenberger <stephan.ellenberger@justeattakeaway.com>
2023-01-09 08:43:02 +0100

    QuizAnswer answer_image_url migrated to s3

--- app/models/quiz_answer.rb
+++ app/models/quiz_answer.rb
@@ -1,5 +1,17 @@
 # frozen_string_literal: true
 
 class QuizAnswer < ApplicationRecord
+  include ::V2::Image::ImageService
+
+  has_one_attached :answer_image_url_s3
+
   belongs_to :quiz_answer, optional: true
+
+  def answer_image_url
+    if answer_image_url_s3.attached?
+      get_image_path(answer_image_url_s3)
+    else
+      super
+    end
+  end
 end
