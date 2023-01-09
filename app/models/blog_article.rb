89f0b511cd4f3a7e712b43dad63d9a7a5b336fe3
Stephan Ellenberger <stephan.ellenberger@justeattakeaway.com>
2023-01-09 08:16:05 +0100

    Migrate blog_article to use s3 header image

--- app/models/blog_article.rb
+++ app/models/blog_article.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class BlogArticle < ApplicationRecord
+  include ::V2::Image::ImageService
+
+  has_one_attached :header_image_s3
+
   belongs_to :user
   belongs_to :campaign_action
   belongs_to :campaign
@@ -10,4 +14,12 @@ class BlogArticle < ApplicationRecord
   has_many :image_sections, dependent: :destroy
   has_many :blog_tags
   has_many :tags, through: :blog_tags
+
+  def header_image
+    if header_image_s3.attached?
+      get_image_path(header_image_s3)
+    else
+      super
+    end
+  end
 end
