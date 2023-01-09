11d09fd527ed490f2c6cc481462d41a470b56006
Stephan Ellenberger <stephan.ellenberger@justeattakeaway.com>
2023-01-09 08:39:58 +0100

    PressCoverageArticle image_url migrated to s3

--- app/models/press_coverage_article.rb
+++ app/models/press_coverage_article.rb
@@ -1,4 +1,15 @@
 # frozen_string_literal: true
 
 class PressCoverageArticle < ApplicationRecord
+  include ::V2::Image::ImageService
+
+  has_one_attached :image_url_s3
+
+  def image_url
+    if image_url_s3.attached?
+      get_image_path(image_url_s3)
+    else
+      super
+    end
+  end
 end
