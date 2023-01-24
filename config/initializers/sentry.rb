2b132d3e5b50fd0fde60c07c26f74fc36e7f7c2d
jelgar <jamesnelgar@gmail.com>
2023-01-24 00:33:39 +0000

    Update sentry to use stage for environment

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,7 +1,7 @@
 Sentry.init do |config|
   config.dsn = ENV['SENTRY_DSN']
 
-  config.environment = Rails.env
+  config.environment = ENV['STAGE'] 
   config.breadcrumbs_logger = [:sentry_logger, :http_logger]
 
   config.traces_sample_rate = 0.01
