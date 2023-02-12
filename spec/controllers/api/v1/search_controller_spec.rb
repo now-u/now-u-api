783ca6e61f1b5e8d968fb352e567fad5c419d3eb
James Elgar <jamesnelgar@gmail.com>
2023-02-12 18:03:23 +0000

    Fix tests

--- spec/controllers/api/v1/search_controller_spec.rb
+++ spec/controllers/api/v1/search_controller_spec.rb
@@ -2,13 +2,13 @@ require 'rails_helper'
 
 RSpec.describe Api::V1::SearchController, type: :controller do
     describe "#search" do
-        let!(:campaign) { Campaign.create!(title: "test campaign")}
-        let!(:campaign2) { Campaign.create!(title: "test", description_web: "campaign")}
-        let!(:learning_topic) { LearningTopic.create!(title: "lt test", campaign: campaign)}
-        let!(:learning_resource) { LearningResource.create!(title: "test lr", learning_topic: learning_topic)}
-        let!(:learning_resource2) { LearningResource.create!(title: "test", type: "lr", learning_topic: learning_topic)}
-        let!(:campaign_action) { CampaignAction.create!(title: "test action", campaign: campaign)}
-        let!(:campaign_action2) { CampaignAction.create!(title: "test", type: "action", campaign: campaign)}
+        let!(:campaign) { FactoryBot.create(:campaign, title: "test campaign")}
+        let!(:campaign2) { FactoryBot.create(:campaign, title: "test", description_web: "campaign")}
+        let!(:learning_topic) { FactoryBot.create(:learning_topic, title: "lt test", campaign: campaign)}
+        let!(:learning_resource) { FactoryBot.create(:learning_resource, title: "test lr", learning_topic: learning_topic)}
+        let!(:learning_resource2) { FactoryBot.create(:learning_resource, title: "test", type: "lr", learning_topic: learning_topic)}
+        let!(:campaign_action) { FactoryBot.create(:campaign_action, title: "test action", campaign: campaign)}
+        let!(:campaign_action2) { FactoryBot.create(:campaign_action, title: "test", type: "action", campaign: campaign)}
 
         it "should return only campaings" do
             get :index, params: { query: "campaign" }, as: :json
