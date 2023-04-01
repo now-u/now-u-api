3683013849135ea2ebdb13093b392ada0255d776
James Elgar <jamesnelgar@gmail.com>
2023-04-01 13:37:30 +0100

    Fix default value for auth_user_id

--- db/migrate/20230225165043_migration_add_user_auth_id.rb
+++ db/migrate/20230225165043_migration_add_user_auth_id.rb
@@ -1,6 +1,6 @@
 class MigrationAddUserAuthId < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, :auth_user_id, :string, deafult: "-", null: false
+    add_column :users, :auth_user_id, :string, default: "-", null: false
     add_index :users, :auth_user_id
   end
 end
